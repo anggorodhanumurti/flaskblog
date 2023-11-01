b7e836b943939d5ecd0bedd152f84910e3e14aa9
anggorodhanumurti <dhanumurtianggoro@gmail.com>
2023-11-01 17:33:44 +0700

    gravatar profile picture

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from app import db
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -23,6 +24,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=robohash&s{}'.format(digest, size)
 
     
 class Post(db.Model):
@@ -33,3 +38,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
